925953be9b96803c03492d3a8e54bb1b30af70de
cruizji <57686387+cruizji@users.noreply.github.com>
2022-11-25 11:14:55 +0100

    Update python_24112022.py

--- python_24112022.py
+++ python_24112022.py
@@ -44,3 +44,16 @@ while aleatorio != usuario:
      print ("Good luck, it´s the same")
     else:
      print("Bad luck")
+     
+########## Leer csv y calcular media
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("access-code.csv")
+print(df.to_string())
+media = df['Identifier'].mean() # Media columna
+Identificador = df['Identifier'].sum() # Suma columna
+
+
+
+

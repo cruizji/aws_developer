62e3f6f7270410132a5154f51ec68d0637b5bb97
cruizji <57686387+cruizji@users.noreply.github.com>
2022-11-25 20:54:18 +0100

    Update ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -23,14 +23,14 @@ acierto = False
 intentos = int(len(adivina) + 1)
 k = 0
 
-print("Adivina la palabra: {}".format(adivina))
+print("La palabra a adivinar tiene {} caracteres".format(len(adivina)))
 while acierto == False and contador <= intentos:
     caracter = input("Introduce un caracter de la palabra: ")
    
     if k == 0:
         for i in adivina : # Inicializar la lista y rellenarla con valores
             lista2.append("*")
-            print(i)
+         #   print(i)
             k = k + 1
         print(lista2)
         
@@ -40,22 +40,23 @@ while acierto == False and contador <= intentos:
             lista2[index]=caracter
         else:
             exito = False
-        print(index)    
+        #print(index)    
         
     if "*" in lista2:  # Chequear que la lista esta completa y se ha adivinado la palabra
         acierto = False
     else:
         acierto = True
+        break
         
     restantes= intentos - contador
     print("estas son las posiciones del caracter: {} y tienes estos intentos {}".format(lista2, restantes )) 
     contador = contador + 1
     if restantes < 1:
-        print ("You are not GOAT, bad luck !!!!!")
+        print ("!!!!!! :( You are not GOAT, bad luck !!!!!, the word to guess was: {}".format(adivina))
         raise SystemExit
       
         
-print("Nice !!!!!, You are GOAT (Greatest of all times")
+print("--------------Nice !!!!!, You are GOAT (Greatest of all times)------------")
         
     
 #print(numero)
